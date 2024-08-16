2b7e7916797e1239f45979a8d0cf1be3b142de40
Fabricio Velasco <fabriciorvelasco@gmail.com>
2024-08-15 21:57:23 -0300

    lista de botões

--- app/src/main/java/com/fabriciovelasco/estudos/MainActivity.kt
+++ app/src/main/java/com/fabriciovelasco/estudos/MainActivity.kt
@@ -5,7 +5,11 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -18,11 +22,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val mainItems = mutableListOf<MainItem>()
+        mainItems.add(
+            MainItem(
+                id = 1,
+                drawableId = R.drawable.baseline_trending_up_24,
+                textStringId = R.string.label_exe606,
+                color = R.color.cinza
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 2,
+                drawableId = R.drawable.baseline_trending_up_24,
+                textStringId = R.string.label_exe607,
+                color = R.color.cinza
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 3,
+                drawableId = R.drawable.baseline_trending_up_24,
+                textStringId = R.string.label_exe606,
+                color = R.color.cinza
+            )
+        )
+        mainItems.add(
+            MainItem(
+                id = 4,
+                drawableId = R.drawable.baseline_trending_up_24,
+                textStringId = R.string.label_exe607,
+                color = R.color.cinza
+            )
+        )
 
-        val adapter = MainAdapter()
+        val adapter = MainAdapter(mainItems)
         rvMain = findViewById(R.id.rv_main)
         rvMain.adapter = adapter
-        rvMain.layoutManager = LinearLayoutManager(this)
+        rvMain.layoutManager = GridLayoutManager(this,3)
 
 //        btn606 = findViewById(R.id.btn_606)
 //
@@ -34,8 +70,9 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-    private inner class MainAdapter: RecyclerView.Adapter<MainViewHolder>(){
+    private inner class MainAdapter(private val mainItems: List<MainItem>) : RecyclerView.Adapter<MainViewHolder>(){
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
+
             val view = layoutInflater.inflate(R.layout.main_item, parent, false)
             val viewHolder = MainViewHolder(view)
             return viewHolder
@@ -43,18 +80,30 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun getItemCount(): Int {
-            return 5
+            return mainItems.size
         }
 
         override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
-
+            val itemCurrent = mainItems[position]
+            holder.bind(itemCurrent)
         }
 
     }
 
 
     // é a classe da célula em si
-    private class MainViewHolder(view: View): RecyclerView.ViewHolder(view){
+    private class MainViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+        fun bind(item: MainItem) {
+            val img: ImageView = itemView.findViewById(R.id.item_img_icon)
+            val name: TextView = itemView.findViewById(R.id.item_text_name)
+            val container: LinearLayout = itemView as LinearLayout
 
+            img.setImageResource(item.drawableId)
+            name.setText(item.textStringId)
+//            container.setBackgroundColor(item.color)
+
+
+
+        }
     }
 }
